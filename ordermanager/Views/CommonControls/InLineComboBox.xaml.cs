8d2df9dbf045f5f07374ec761151bac3be478db1
venkateshmani <venkatesh@thespark.mygbiz.com>
2013-09-17 20:25:49 +0530

    Added Styles, WPF Toolkit DLLs
    And Added Inline Combo Box Styles

---
+++ ordermanager/Views/CommonControls/InLineComboBox.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ordermanager.Views.CommonControls
+{
+    /// <summary>
+    /// Interaction logic for InLineComboBox.xaml
+    /// </summary>
+    public partial class InLineComboBox : UserControl
+    {
+        public InLineComboBox()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            comboxBox.IsDropDownOpen = true;   
+        }
+    }
+
+    public static class Watermark
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.RegisterAttached("Text",
+                                                 typeof(string),
+                                                 typeof(Watermark),
+                                                 new FrameworkPropertyMetadata());
+
+        public static void SetText(UIElement element, string value)
+        {
+            element.SetValue(TextProperty, value);
+        }
+
+        public static Boolean GetText(UIElement element)
+        {
+            return (Boolean)element.GetValue(TextProperty);
+        }
+    }
+}
---
+++ ordermanager/Views/Extensions/WaterMarkText.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace ordermanager.Views.Extensions
+{
+    public static class Watermark
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.RegisterAttached("Text",
+                                                 typeof(Boolean),
+                                                 typeof(Watermark),
+                                                 new FrameworkPropertyMetadata());
+
+        public static void SetText(UIElement element, Boolean value)
+        {
+            element.SetValue(TextProperty, value);
+        }
+
+        public static Boolean GetText(UIElement element)
+        {
+            return (Boolean)element.GetValue(TextProperty);
+        }
+    }
+}
