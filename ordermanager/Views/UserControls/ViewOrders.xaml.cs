e3a47d66ebca27282a38be8b0fe09929ae667c25
venkateshmani <venkatesh@thespark.mygbiz.com>
2013-09-17 08:58:08 +0530

    Created UserControls

---
+++ ordermanager/Views/UserControls/ViewOrders.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ordermanager.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ViewOrders.xaml
+    /// </summary>
+    public partial class ViewOrders : UserControl
+    {
+        public ViewOrders()
+        {
+            InitializeComponent();
+        }
+    }
+}
