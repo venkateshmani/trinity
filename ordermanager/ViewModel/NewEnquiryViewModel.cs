83359ab1ff70a71ffa1d2e94a1892f10bc1587f4
venkateshmani <venkatesh@thespark.mygbiz.com>
2013-09-25 18:45:41 +0530

    Checking in NewEnquiryVM

---
+++ ordermanager/ViewModel/NewEnquiryViewModel.cs
@@ -0,0 +1,39 @@
+﻿using ordermanager.DatabaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordermanager.ViewModel
+{
+    public class NewEnquiryViewModel
+    {
+        public NewEnquiryViewModel()
+        {
+
+        }
+
+        private Order m_Order = null;
+        public Order Order
+        {
+            get
+            {
+                if (m_Order == null)
+                {
+                    
+                }
+                return m_Order;
+            }
+            set
+            {
+                m_Order = value;
+            }
+        }
+
+        public void Save()
+        {
+
+        }
+    }
+}
