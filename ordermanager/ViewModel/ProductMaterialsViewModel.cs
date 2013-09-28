0e48ff804ec8ad34b0a9e99f6cea6f8324a42992
VeeraBolla <connect.bolla@gmail.com>
2013-09-28 13:08:00 -0700

    Added code to add new material

--- ordermanager/ViewModel/ProductMaterialsViewModel.cs
+++ ordermanager/ViewModel/ProductMaterialsViewModel.cs
@@ -14,6 +14,7 @@ namespace ordermanager.ViewModel
         private Order m_Order = null;
         private OrderProduct m_SelectedItem = null;
         private ObservableCollection<OrderProduct> m_Products = null;
+        ObservableCollection<ProductMaterial> m_SelectedProductMaterials;
 
         public ObservableCollection<OrderProduct> Products
         {
@@ -25,7 +26,20 @@ namespace ordermanager.ViewModel
             }
         }
 
-        public OrderProduct SelectedItem
+        public ObservableCollection<ProductMaterial> ProductMaterialsList
+        {
+            get
+            {
+                if (m_SelectedProductMaterials == null)
+                {
+                    m_SelectedProductMaterials = new ObservableCollection<ProductMaterial>(m_SelectedItem.ProductMaterials);
+                    m_SelectedProductMaterials.CollectionChanged += m_SelectedProductMaterials_CollectionChanged;
+                }
+                return m_SelectedProductMaterials;
+            }
+        }
+
+        public OrderProduct SelectedProduct
         {
             get { return m_SelectedItem; }
             set
@@ -33,7 +47,7 @@ namespace ordermanager.ViewModel
                 if (value != m_SelectedItem)
                 {
                     m_SelectedItem = value;
-                    NotifyPropertyChanged("SelectedItem");
+                    NotifyPropertyChanged("SelectedProduct");
                 }
             }
         }
@@ -41,7 +55,7 @@ namespace ordermanager.ViewModel
         public bool SetOrder(Order order)
         {
             if (m_Order != order)
-            {                
+            {
                 if (order != null)
                 {
                     Products = new ObservableCollection<OrderProduct>(order.OrderProducts);
@@ -50,6 +64,31 @@ namespace ordermanager.ViewModel
             }
             return true;
         }
+
+        public MaterialName CreateNewMaterial(string materialName)
+        {
+            return DBResources.Instance.CreateNewMaterial(materialName);
+        }
+        
+        void m_SelectedProductMaterials_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                foreach (var newItem in e.NewItems)
+                {
+                    ProductMaterial newMaterialItem = newItem as ProductMaterial;
+                    newMaterialItem.OrderProduct = m_SelectedItem;                   
+                }
+            }
+            else if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Remove)
+            {
+                foreach (var deletedItem in e.OldItems)
+                {
+                    ProductMaterial deletedMaterial = deletedItem as ProductMaterial;
+                    deletedMaterial.OrderProduct = null;                   
+                }
+            }
+        }           
 
         #region [INotifyPropertyChanged]
         public event PropertyChangedEventHandler PropertyChanged;
--- ordermanager/Views/UserControls/ProductMaterialsControl.xaml.cs
+++ ordermanager/Views/UserControls/ProductMaterialsControl.xaml.cs
@@ -21,17 +21,50 @@ namespace ordermanager.Views.UserControls
     /// Interaction logic for ProductMaterials.xaml
     /// </summary>
     public partial class ProductMaterialsControl : UserControl
-    {       
+    {
+        ProductMaterialsViewModel m_ViewModel;
+
         public ProductMaterialsControl()
         {
-            InitializeComponent();           
-        }             
+            InitializeComponent();
+        }
 
         private void productsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             OrderProduct product = productsList.SelectedItem as OrderProduct;
-            if (product != null && this.DataContext != null)
-                (DataContext as ProductMaterialsViewModel).SelectedItem = product;
+            if (product != null && m_ViewModel != null)
+                m_ViewModel.SelectedProduct = product;
+        }
+
+        private void AddNewMaterial(object sender, RoutedEventArgs e)
+        {
+            Button addBtn = sender as Button;
+            if (addBtn != null)
+            {
+                Grid parentGrid = addBtn.Parent as Grid;
+                if (parentGrid != null)
+                {
+                    ComboBox comboBox = parentGrid.FindName("materialsComboBox") as ComboBox;
+                    if (comboBox != null && m_ViewModel != null)
+                    {
+                        comboBox.SelectedItem = m_ViewModel.CreateNewMaterial(comboBox.Text);
+                        addBtn.Visibility = System.Windows.Visibility.Collapsed;
+                    }
+                }
+            }
+        }
+
+        private void UserControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            m_ViewModel = DataContext as ProductMaterialsViewModel;
+        }
+
+        private void AddNewItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel != null)
+            {
+                m_ViewModel.ProductMaterialsList.Add(new ProductMaterial());
+            }
         }
     }
 }
