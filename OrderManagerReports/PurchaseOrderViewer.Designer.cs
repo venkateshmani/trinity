709083c60c6fb4327b641373f4eaff618f671c5d
venkateshmani <venkatesh@thespark.mygbiz.com>
2013-10-06 16:22:53 +0530

    Added Report Project

---
+++ OrderManagerReports/PurchaseOrderViewer.Designer.cs
@@ -0,0 +1,95 @@
+﻿//------------------------------------------------------------------
+// <copyright company="Microsoft">
+//     Copyright (c) Microsoft.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------
+namespace OrderManagerReports
+{
+    partial class PurchaseOrderViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.OrderManagerDBDataSet = new OrderManagerReports.OrderManagerDBDataSet();
+            this.ProductMaterialItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ProductMaterialItemTableAdapter = new OrderManagerReports.OrderManagerDBDataSetTableAdapters.ProductMaterialItemTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.OrderManagerDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductMaterialItemBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "MaterialsToPurchase";
+            reportDataSource1.Value = this.ProductMaterialItemBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "OrderManagerReports.PurchaseOrderReport.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(682, 386);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // OrderManagerDBDataSet
+            // 
+            this.OrderManagerDBDataSet.DataSetName = "OrderManagerDBDataSet";
+            this.OrderManagerDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // ProductMaterialItemBindingSource
+            // 
+            this.ProductMaterialItemBindingSource.DataMember = "ProductMaterialItem";
+            this.ProductMaterialItemBindingSource.DataSource = this.OrderManagerDBDataSet;
+            // 
+            // ProductMaterialItemTableAdapter
+            // 
+            this.ProductMaterialItemTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 386);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OrderManagerDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductMaterialItemBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource ProductMaterialItemBindingSource;
+        private OrderManagerDBDataSet OrderManagerDBDataSet;
+        private OrderManagerDBDataSetTableAdapters.ProductMaterialItemTableAdapter ProductMaterialItemTableAdapter;
+    }
+}
+
---
+++ OrderManagerReports/PurchaseOrderViewer.cs
@@ -0,0 +1,32 @@
+﻿//------------------------------------------------------------------
+// <copyright company="Microsoft">
+//     Copyright (c) Microsoft.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrderManagerReports
+{
+    public partial class PurchaseOrderViewer : Form
+    {
+        public PurchaseOrderViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'OrderManagerDBDataSet.ProductMaterialItem' table. You can move, or remove it, as needed.
+            this.ProductMaterialItemTableAdapter.Fill(this.OrderManagerDBDataSet.ProductMaterialItem);
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
