85529442de22a1a42316186b37a4bc9527c14b3e
venkateshmani <venkatesh@thespark.mygbiz.com>
2013-11-08 20:44:56 +0530

    Revert "INR Fix"
    This reverts commit 2c4e4af61f5724a4198a523ccb83cabc2b244258.

--- ordermanager/Extended Database Models/ProductMaterialItem.cs
+++ ordermanager/Extended Database Models/ProductMaterialItem.cs
@@ -342,7 +342,7 @@ namespace ordermanager.DatabaseModel
                 if (currencyConversion == null || currencyConversion.Count() == 0)
                 {
                     newConversion = new OrderCurrencyConversion();
-                    newConversion.CurrencyWrapper = Currency;
+                    newConversion.Currency = Currency;
 
                     ProductMaterial.OrderProduct.Order.OrderCurrencyConversions.Add(newConversion);
                 }
