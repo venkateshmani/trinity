7979c7c903dd0a4b81197d22bedd299e9ae112b3
venkateshmani <venkatesh@thespark.mygbiz.com>
2013-11-10 18:03:49 +0530

    Added Job Execution Controls

---
+++ ordermanager/Views/UserControls/Execution/QualityControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ordermanager.Views.UserControls.Execution
+{
+    /// <summary>
+    /// Interaction logic for QualityControl.xaml
+    /// </summary>
+    public partial class QualityControl : UserControl
+    {
+        public QualityControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExecutionTreeViewControl_SelectionChanged(DatabaseModel.OrderProduct product, DateTime date)
+        {
+
+        }
+    }
+}
---
+++ ordermanager/Views/UserControls/Execution/ShippingControl.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ordermanager.Views.UserControls.Execution
+{
+    /// <summary>
+    /// Interaction logic for ShippingControl.xaml
+    /// </summary>
+    public partial class ShippingControl : UserControl
+    {
+        public ShippingControl()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExecutionTreeViewControl_SelectionChanged(DatabaseModel.OrderProduct product, DateTime date)
+        {
+
+        }
+    }
+}
