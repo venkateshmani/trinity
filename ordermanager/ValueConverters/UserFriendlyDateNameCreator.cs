64bfae7de946ac253e38696758bbbddc2eb942a2
venkateshmani <venkatesh@thespark.mygbiz.com>
2013-11-10 20:55:11 +0530

    UserFriendlyDateNameCreator

---
+++ ordermanager/ValueConverters/UserFriendlyDateNameCreator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ordermanager.ValueConverters
+{
+    public class UserFriendlyDateNameCreator : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is string)
+            {
+                try
+                {
+                    DateTime dt = DateTime.Parse((string)value);
+                    if (dt.ToShortDateString() == DateTime.Now.ToShortDateString())
+                    {
+                        return "Today";
+                    }
+                }
+                catch
+                {
+                     //Keep Quite                            
+                }
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
