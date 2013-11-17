0111b2627b6862f7d60b47e98e3da179ffef2df6
VeeraBolla <connect.bolla@gmail.com>
2013-11-17 12:47:07 +0530

    Service changes

---
+++ AssetManager/AssetManagerWebService/AssetManagerService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using AssetManagerWebService.DatabaseModel;
+
+namespace AssetManagerWebService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class AssetManagerService : IAssetManagerService
+    {
+
+        public LoginResult Authenticate(string userName, string password)
+        {
+            AssetManagerDBEntities dbContext = new AssetManagerDBEntities();
+            List<UserName> users = dbContext.UserNames.ToList();
+            if (users != null && users.Count > 0)
+            {
+                UserName user = dbContext.UserNames.Where(u => u.Name == userName).Select(u => u)
+                                                         .FirstOrDefault();
+                if (user != null)
+                {
+                    string passwordValueInDatabase = user.Password;
+                    if (user.Password == password)
+                        return new LoginResult() { Authenticated = true, NeedPasswordReset = (passwordValueInDatabase == user.Name) };
+                    else
+                        return new LoginResult() { Authenticated = false, Message = "Authentication failed." };
+                }
+                return new LoginResult() { Authenticated = false, Message = "User not found" };
+            }
+            return new LoginResult() { Authenticated = false, Message = "User not found" };
+        }
+    }
+}
---
+++ AssetManager/AssetManagerWebService/IAssetManagerService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace AssetManagerWebService
+{  
+    [ServiceContract]
+    public interface IAssetManagerService
+    {       
+
+        [OperationContract]
+        LoginResult Authenticate(string userName, string password);
+
+
+    }
+
+    [DataContract]
+    public class LoginResult
+    {
+        [DataMember]
+        public bool Authenticated
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string Message
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public bool NeedPasswordReset
+        {
+            get;
+            set;
+        }
+
+    }
+}
