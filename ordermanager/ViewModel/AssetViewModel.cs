7f11e19ccffba10f6cf89423d0d4bbcb26505e5a
venkateshmani <venkatesh@thespark.mygbiz.com>
2013-11-18 08:10:20 +0530

    Added Asset View Model

--- ordermanager/ViewModel/AssetViewModel.cs
+++ ordermanager/ViewModel/AssetViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ordermanager.DatabaseModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,37 @@ namespace ordermanager.ViewModel
 {
     public class AssetViewModel
     {
+        public AssetViewModel()
+        {
+
+        }
+
+        private ObservableCollection<Asset> m_ExistingAssets = null;
+        public ObservableCollection<Asset> ExistingAssets
+        {
+            get
+            {
+                if (m_ExistingAssets == null)
+                {
+                    m_ExistingAssets = new ObservableCollection<Asset>(DBResources.Instance.Context.Assets.ToList());
+                }
+
+                return m_ExistingAssets;
+            }
+        }
+
+        public Asset AddNewAsset(Asset asset)
+        {
+            Asset newlyAddedAsset = DBResources.Instance.Context.Assets.Add(asset);
+            DBResources.Instance.Save();
+
+            if (ExistingAssets != null)
+            {
+                ExistingAssets.Add(newlyAddedAsset);
+            }
+
+            return newlyAddedAsset;
+        }
+
     }
 }
