1ccd349b71642ee4fecce58480febbe52127ebcc
Venkatesh Mani Selvaraj <venzzoft@gmail.com>
2013-11-30 15:53:30 +0530

    Job work control

--- ordermanager/Views/UserControls/JobWorkControl.xaml.cs
+++ ordermanager/Views/UserControls/JobWorkControl.xaml.cs
@@ -23,11 +23,71 @@ namespace ordermanager.Views.UserControls
         public JobWorkControl()
         {
             InitializeComponent();
+            List<Supplier> supp = new List<Supplier>();
+            supp.Add(new Supplier());
+            supp.Add(new Supplier());
+            supp.Add(new Supplier());
+            tvSuppliers.ItemsSource = supp;
         }
 
         private void Save_Click(object sender, RoutedEventArgs e)
         {
+            
+        }
+    }
 
+
+    public class Supplier
+    {
+        static int count = 0;
+        public Supplier()
+        {
+            count++;
+            PurchaseOrders = new List<PurchaseOrderTemp>();
+            Name = "Supplier" + count.ToString();
+            PurchaseOrders.Add(new PurchaseOrderTemp());
+            PurchaseOrders.Add(new PurchaseOrderTemp());
+            PurchaseOrders.Add(new PurchaseOrderTemp());
+        }
+        public string Name
+        { get; set; }
+        public List<PurchaseOrderTemp> PurchaseOrders
+        { get; set; }
+    }
+
+    public class PurchaseOrderTemp
+    {
+        static int count = 0;
+        public PurchaseOrderTemp()
+        {
+            count++;
+            Receipts = new List<Receipt>();
+            Name = "PurchaseOrder" + count.ToString();
+            Receipts.Add(new Receipt());
+            Receipts.Add(new Receipt());
+            Receipts.Add(new Receipt());
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public List<Receipt> Receipts
+        { get; set; }
+    }
+
+    public class Receipt
+    {
+        static int count = 0;
+        public Receipt()
+        {
+            count++;
+            Name = "Receipt" + count.ToString();
+        }
+        public string Name
+        {
+            get;
+            set;
         }
     }
 }
